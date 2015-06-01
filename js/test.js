13561dabfafcfdee2f77d564c3fe8b9bd84824cf
Monkey Wonder <monkey.wonder@voodoo-sl.com>
2015-06-01 01:18:10 +0100

    testing compass

--- js/test.js
+++ js/test.js
@@ -1,5 +1,21 @@
-﻿$(function () {
+﻿function onSuccess(heading) {
+    $('#heading').text(heading.magneticHeading);
+    setTimeout(checkCompass(), 1000);
+};
+
+function onError(error) {
+    setTimeout(checkCompass(), 1000);
+};
+
+function checkCompass()
+{
+    navigator.compass.getCurrentHeading(onSuccess, onError);
+}
+
+$(function () {
     $('#btn').click(function () {
         navigator.vibrate(3000);
     });
+
+    setTimeout(checkCompass(), 1000);
 });
